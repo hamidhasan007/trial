cc2c9c6fdee2c6908f3364f89d583a6fef0c4204
Md Hamid Hasan <113001640+hamidhasan007@users.noreply.github.com>
2022-12-26 10:39:53 +0530

    Update hamid.js

--- hamid.js
+++ hamid.js
@@ -1,5 +1,5 @@
 let char = a;
  if (char==a && char==i && char==e && char==o && char==u){ console.log( "Vowels")}
 else {
-  ("consonat, number or special charactor")
+  ("consonat, symbol, number or special charactor")
 }
